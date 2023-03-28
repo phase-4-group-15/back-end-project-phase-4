b29cd473a3f58adda85676bc416f0c73500b23f1
¨kericho¨ <lvisrono90@gmail.com>
2023-03-28 12:47:20 +0300

    create user

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,16 @@
+class UsersController < ApplicationController
+
+    # def register 
+    #     user = User.create(user_params)
+    #     if user.valid?
+    #         save_user(user.id)
+    #         app_response(message: 'Registration was successfull', data: user)
+    #     else
+    #         app_response(message: 'Something went wrong during registration', status: :unprocessable_entity, data: user.errors)
+    #     end
+    # end
+
+    # def login
+
+    # end
+end
