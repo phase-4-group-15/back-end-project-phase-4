64c08654cca52ce442044f6c8173e600f845bee1
NyokabiKamau <nyokabikamau2014@gmail.com>
2023-04-02 23:12:15 +0300

    added reset password

--- config/routes.rb
+++ config/routes.rb
@@ -5,8 +5,10 @@ Rails.application.routes.draw do
   get '/me', to: "users#show"
   post '/login', to: "sessions#create"
   delete '/logout', to: "sessions#destroy"
+  patch '/reset/:id', to: 'users#reset_password'
   
 
+  # articles
   post '/articles', to: 'articles#create'
   get '/articles', to: 'articles#index'
   get '/articles/:id', to: 'articles#show'
@@ -15,30 +17,9 @@ Rails.application.routes.draw do
   post '/articles/:id/like', to: 'articles#like'
   post '/articles/:id/dislike', to: 'articles#dislike'
   
-  patch '/reset/:id', to: 'users#reset_password'
 
   # verify auth
   get '/verify', to: 'application#verify_auth'
-
-
-  # get '/me/:id', to: 'users#show'
-  # post '/users/signup', to: 'users#create'
-  # post '/users/login', to: 'sessions#create'
-  # delete '/users/logout', to: 'sessions#destroy'
-  # get '/users/articles', to: 'users#index'
-  # get '/users/reviews', to: 'users#reviews'
-  
-
-  # article
-  # resources :articles
-  
-  # resources :articles, only:[:index, :show] do
-  #   resources :reviews, only:[:index, :update, :create]
-  # end
-  
-  # resources :users, only: [:show] do
-  #   resources :articles, only: [:index]
-  # end
 
   # author
   get '/authors', to: 'authors#author_index'
@@ -47,7 +28,6 @@ Rails.application.routes.draw do
   post '/authors/signup', to: 'authors#create'
   post '/authors/login', to: 'authors#login'
   delete '/authors/logout', to: 'authors#logout'
- 
 
   # reviews
   resources :reviews
