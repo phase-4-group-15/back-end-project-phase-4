ef5be981d34eb2fe3923f567e911a296df0d06ea
NyokabiKamau <nyokabikamau2014@gmail.com>
2023-04-03 09:40:49 +0300

    push

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
   get '/me', to: "users#show"
   post '/login', to: "sessions#create"
   delete '/logout', to: "sessions#destroy"
-  patch '/reset/:id', to: 'users#reset_password'
+  put '/reset/:id', to: 'users#reset_password'
   
 
   # articles
