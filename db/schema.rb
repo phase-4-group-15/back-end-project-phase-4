003c41c1ce8fc12d8dde3f1722f6999641928d65
NyokabiKamau <nyokabikamau2014@gmail.com>
2023-04-03 11:24:26 +0300

    reset schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_04_03_031616) do
+ActiveRecord::Schema[7.0].define(version: 2023_04_03_081934) do
   create_table "advertisements", force: :cascade do |t|
     t.text "image", null: false
     t.string "title", null: false
@@ -35,37 +35,24 @@ ActiveRecord::Schema[7.0].define(version: 2023_04_03_031616) do
     t.index ["user_id"], name: "index_articles_on_user_id"
   end
 
-  create_table "authors", force: :cascade do |t|
-    t.string "name", null: false
-    t.text "email", null: false
-    t.string "password_digest", null: false
-    t.text "bio", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  create_table "change_author", force: :cascade do |t|
-  end
-
   create_table "reviews", force: :cascade do |t|
+    t.integer "rating", null: false
+    t.text "comment", null: false
     t.integer "article_id", null: false
     t.integer "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.integer "rating"
-    t.text "comment"
     t.index ["article_id"], name: "index_reviews_on_article_id"
     t.index ["user_id"], name: "index_reviews_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
     t.string "username", null: false
-    t.text "email", null: false
+    t.string "email", null: false
     t.string "password_digest", null: false
     t.text "bio", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "password_confirmation"
   end
 
   add_foreign_key "advertisements", "users"
